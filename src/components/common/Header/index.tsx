c77aa921eba5f96509f5950251115ea0a49d94a8
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-04-25 09:00:25 -0300

    Add images

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,7 +1,5 @@
 import './Header.scss'
-import MobileMenu from './MobileMenu'
-import DesktopMenu from './DesktopMenu'
-import logo from '@/assets/images/logo.png'
+import logo from '@/assets/images/21.png'
 
 const Header = () => {
   return (
@@ -10,12 +8,6 @@ const Header = () => {
         <a href="/">
           <img src={logo} alt="Logo" width={150} height={150} />
         </a>
-      </div>
-      <div className="header__desktop">
-        <DesktopMenu />
-      </div>
-      <div className="header__mobile">
-        <MobileMenu />
       </div>
     </header>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,9 +6,58 @@ const Home = () => {
   return (
     <div className="containerHome">
       <Header />
-      <main className='containerHome__content'>
+      <main className="containerHome__content">
         <h1>Bem vindo ao Snow Pet Shop</h1>
-        <h2>Onde amor e cuidado encontram um lar <em>peludo</em>.</h2>
+        <h2>
+          Onde amor e cuidado encontram um lar <em>peludo</em>.
+        </h2>
+        <div>
+          <table>
+            <tr>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div>
+          <table>
+            <tr>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+              <td>
+                <a href="">
+                  <img src="" alt="" />
+                </a>
+              </td>
+            </tr>
+          </table>
+        </div>
       </main>
       <Footer />
     </div>
