b47dbefdd14f94f74df070716e414e7f0e43e726
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-04-25 12:11:12 -0300

    Finish homepage
    Finalizei a página Home, modifiquei algumas imagens e tornei responsivo os componentes footer e header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import './Header.scss'
-import logo from '@/assets/images/21.png'
+import logo from '@/assets/images/logo_rounded.png'
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__logo">
         <a href="/">
-          <img src={logo} alt="Logo" width={150} height={150} />
+          <img src={logo} alt="Logo Snow Pet" width={150} height={150} />
         </a>
       </div>
     </header>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,62 +1,35 @@
 import './Home.scss'
-import Footer from '@/components/common/Footer'
-import Header from '@/components/common/Header'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { Link } from 'react-router-dom';
+
+const options = [
+  { icon: 'banho_e_tosa', label: 'Banho e tosa', route: '/banho_e_tosa' },
+  { icon: 'cliente', label: 'Cliente', route: '/cliente' },
+  { icon: 'estoque', label: 'Estoque', route: '/estoque' },
+  { icon: 'financeiro', label: 'Financeiro', route: '/financeiro' },
+  { icon: 'funcionario', label: 'Funcionario', route: '/funcionario' },
+  { icon: 'pet', label: 'Pet', route: '/pet' },
+  { icon: 'produto', label: 'Produto', route: '/produto' },
+  { icon: 'relatorio', label: 'Relatorio', route: '/relatorio' },
+  { icon: 'venda', label: 'Venda', route: '/venda' },
+];
 
 const Home = () => {
   return (
     <div className="containerHome">
       <Header />
       <main className="containerHome__content">
-        <h1>Bem vindo ao Snow Pet Shop</h1>
-        <h2>
+        <h1 className="content__title">Bem vindo ao Snow Pet Shop</h1>
+        <h2 className="content__subtitle">
           Onde amor e cuidado encontram um lar <em>peludo</em>.
         </h2>
-        <div>
-          <table>
-            <tr>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-            </tr>
-          </table>
-        </div>
-        <div>
-          <table>
-            <tr>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-              <td>
-                <a href="">
-                  <img src="" alt="" />
-                </a>
-              </td>
-            </tr>
-          </table>
+        <div className='content__options'>
+          {options.map((option, index) => (
+            <Link key={index} to={option.route} >
+              <img src={`/src/assets/images/${option.icon}.png`} alt={option.label} className='options__icon'/>
+            </Link>
+          ))}
         </div>
       </main>
       <Footer />
