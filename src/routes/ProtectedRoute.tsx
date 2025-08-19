e89d56f3ad183a98a260625edbe17b997b08bfd7
viniciussis <vinicius01012@gmail.com>
2025-08-19 11:52:05 -0300

    Update ProtectedRoute to read token from useAuthStore

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom'
 import Loading from '@/components/Loading'
 
 export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const token = useAuthStore((s) => s.token)
+  const { token } = useAuthStore();
   const { isPending, isError } = useAuth()
 
   if (!token) return <Navigate to="/auth/signin" />
