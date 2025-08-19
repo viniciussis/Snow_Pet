79136cfb0b40226697d36b5e21ae5fa759a03619
viniciussis <vinicius01012@gmail.com>
2025-08-19 11:51:58 -0300

    Enhance Header: adjust layout, add padding, flex styling, and logout button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import { useNavigate } from 'react-router-dom'
+
 import logo from '/images/logo_rounded.png'
+import Button from '../Button'
 import './Header.scss'
+import { useAuthStore } from '@/hooks/stores'
 
 const Header = () => {
   const navigate = useNavigate()
+  const { logout } = useAuthStore();
   return (
     <header className="header">
       <img
@@ -14,6 +18,7 @@ const Header = () => {
         width={100}
         src={logo}
       />
+      <Button text='Sair' colorType='fail' onClick={logout} />
     </header>
   )
 }
